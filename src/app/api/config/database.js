ed385296dccfb049a9896e77a467a3f0afa5d808
Javor Emir <emir.j2002@gmail.com>
2025-01-16 16:30:14 +0100

    add WebSocket user management and database configuration

--- src/app/api/config/database.js
+++ src/app/api/config/database.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.resolve(__dirname, '../../../../.env') });
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
--- src/app/api/server.js
+++ src/app/api/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
 
-
+const database = require('./config/database');
 
 const app = express();
 const server = http.createServer(app);
